d7f8840191b41b3b2adadcf208f831d3e9234496
Đoàn Thanh Lực <53224527+thanhlucvip2@users.noreply.github.com>
2023-10-15 22:09:54 +0700

    test

--- server.js
+++ server.js
@@ -7,50 +7,15 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  let config = {
-    method: "get",
-    maxBodyLength: Infinity,
-    url: "https://api.my-ip.io/ip.json",
-    headers: {},
+app.get("/", (req, res) => {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
   };
 
-  setInterval(() => {
-    const now = dayjs();
-    axios
-      .request(config)
-      .then((response) => {
-        postDAta({
-          ip: response.data.ip,
-          date: now.format("YYYY-MM-DD HH:mm"),
-          name: "server 4",
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, 10000);
+  fetch("https://api.myip.com/", requestOptions)
+    .then((response) => response.json())
+    .then((result) => res.json(result))
+    .catch((error) => console.log("error", error));
 });
-
-const postDAta = (datasss) => {
-  let data = qs.stringify(datasss);
-
-  let config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: "https://61a897c033e9df0017ea39cd.mockapi.io/api/ver1/ip",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    data: data,
-  };
-
-  axios
-    .request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+app.listen(PORT, () => {});
